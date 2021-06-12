72b47abe819d53ddc10707151fbfa0a7240ef146
cadu1996 <carlos.eduardo.gba@gmail.com>
2021-06-12 02:42:37 -0300

    Implementation record() function

--- LifeLike.py
+++ LifeLike.py
@@ -16,7 +16,7 @@ grid = np.random.randint(2, size=(270,480), dtype=np.uint8)
 
 
 def next_generation():
-    global grid
+    global grid 
 
     ### Create a kernel ###
     kernel = np.ones((3,3))
@@ -31,3 +31,27 @@ def next_generation():
     grid[(old_grid == 0) & (np.isin(neighbours,born))] = 1
 
     return grid
+
+def record(title="teste.mp4",videodims=(1920,1080), fps=10, generation=100):
+    ### Inicialization Record Video ###
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')                        ## Define Codec to MP4
+    video = cv2.VideoWriter(title, fourcc, fps, videodims)
+
+    bgr = np.zeros(grid.shape, dtype=np.uint8)
+    ### Loop to record frame in video ###
+    for i in range(generation):
+        next_generation()                                      
+        
+        img = np.dstack((grid*255, bgr, bgr))
+
+        img = cv2.resize(img, videodims)                            ## Resize image to 1920x1080
+
+        ### Write generation in video ###
+        font = cv2.FONT_HERSHEY_SIMPLEX             
+        cv2.putText(img, "generation: {0}".format(i+1),(0,100), font, 2, (255,255,255), 2, cv2.LINE_AA)
+
+        ### Write image in video ###
+        video.write(img)
+    
+    ### Save video ###
+    video.release()
